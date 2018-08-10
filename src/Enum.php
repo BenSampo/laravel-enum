845343ae999a504479a9140bbedd5baf17da559e
Ben Sampson <ben@sampo.co.uk>
2018-08-10 17:57:22 +0100

    Added doc blocks

--- src/Enum.php
+++ src/Enum.php
@@ -6,8 +6,18 @@ use ReflectionClass;
 
 abstract class Enum
 {
+    /**
+     * Constants cache
+     *
+     * @var array
+     */
     private static $constCacheArray = [];
 
+    /**
+     * Get all of the constants on the class
+     *
+     * @return array
+     */
     private static function getConstants(): array
     {
         $calledClass = get_called_class();
@@ -20,44 +30,87 @@ abstract class Enum
         return self::$constCacheArray[$calledClass];
     }
 
+    /**
+     * Get all of the enum keys
+     *
+     * @return array
+     */
     public static function getKeys(): array
     {
         return array_keys(self::getConstants());
     }
 
+    /**
+     * Get all of the enum values
+     *
+     * @return array
+     */
     public static function getValues(): array
     {
         return array_values(self::getConstants());
     }
 
+    /**
+     * Get the key for a single enum value
+     *
+     * @param mixed $value
+     * @return string
+     */
     public static function getKey($value): string
     {
         return array_search($value, self::getConstants(), true);
     }
 
+    /**
+     * Get the value for a single enum key
+     *
+     * @param string $key
+     * @return void
+     */
     public static function getValue(string $key)
     {
         return self::getConstants()[$key];
     }
 
+    /**
+     * Get the description for an enum value
+     *
+     * @param mixed $value
+     * @return string
+     */
     public static function getDescription($value): string
     {
         $key = self::getKey($value);
         return ucfirst(str_replace('_', ' ', snake_case($key)));
     }
 
+    /**
+     * Get a random key from the enum
+     *
+     * @return string
+     */
     public static function getRandomKey(): string
     {
         $keys = self::getKeys();
         return $keys[array_rand($keys)];
     }
 
+    /**
+     * Get a random value from the enum
+     *
+     * @return string
+     */
     public static function getRandomValue(): string
     {
         $values = self::getValues();
         return $values[array_rand($values)];
     }
 
+    /**
+     * Return the enum as an array
+     *
+     * @return array
+     */
     public static function toArray(): array
     {
         return self::getConstants();
