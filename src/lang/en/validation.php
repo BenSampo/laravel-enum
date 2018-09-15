73f5c27f09844bc4c6bf99d937b18f8f6646509b
wi1dcard <wi1dcard@qq.com>
2018-09-15 18:00:13 +0800

    Add validation translations.

---
+++ src/lang/en/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'enum_key' => 'The key you have entered is invalid.',
+    'enum_value' => 'The value you have entered is invalid.',
+];
---
+++ src/lang/zh-CN/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'enum_key' => '枚举键名无效。',
+    'enum_value' => '枚举键值无效。',
+];
