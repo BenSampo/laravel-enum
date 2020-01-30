2e79f0041f7359e1ac5b666148a7530fc2b55186
Benedikt Franke <benedikt.franke@mll.com>
2020-01-30 15:17:08 +0100

    Fix compatibility with PHPStan 0.12

--- src/PHPStan/EnumMethodReflection.php
+++ src/PHPStan/EnumMethodReflection.php
@@ -6,6 +6,7 @@ use PHPStan\Reflection\ClassMemberReflection;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\FunctionVariant;
 use PHPStan\Reflection\MethodReflection;
+use PHPStan\TrinaryLogic;
 use PHPStan\Type\ObjectType;
 
 class EnumMethodReflection implements MethodReflection
@@ -65,5 +66,40 @@ class EnumMethodReflection implements MethodReflection
                 new ObjectType($this->classReflection->getName())
             ),
         ];
+    }
+
+    public function getDocComment(): ?string
+    {
+        return null;
+    }
+
+    public function isDeprecated(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function getDeprecatedDescription(): ?string
+    {
+        return null;
+    }
+
+    public function isFinal(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function isInternal(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function getThrowType(): ?\PHPStan\Type\Type
+    {
+        return null;
+    }
+
+    public function hasSideEffects(): \PHPStan\TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
     }
 }
