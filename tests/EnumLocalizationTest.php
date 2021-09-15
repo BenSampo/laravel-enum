17f3cd1532492a1264a8906835c0539a968b667c
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-09-15 14:25:23 -0400

    Add tests

--- tests/EnumLocalizationTest.php
+++ tests/EnumLocalizationTest.php
@@ -2,6 +2,9 @@
 
 namespace BenSampo\Enum\Tests;
 
+use BenSampo\Enum\Rules\Enum;
+use BenSampo\Enum\Rules\EnumKey;
+use BenSampo\Enum\Tests\Enums\UserType;
 use BenSampo\Enum\Tests\Enums\UserTypeLocalized;
 
 class EnumLocalizationTest extends ApplicationTestCase
@@ -22,5 +25,55 @@ class EnumLocalizationTest extends ApplicationTestCase
 
         $this->app->setLocale('es');
         $this->assertSame('Moderator', UserTypeLocalized::getDescription(UserTypeLocalized::Moderator));
+    }
+
+    public function test_can_localize_validation_error_message_using_class_rule()
+    {
+        $validator = $this->app['validator'];
+
+        $this->assertSame(
+            'The value you have provided is not a valid enum instance.',
+            $validator->make(['input' => 'test'], ['input' => new Enum(UserType::class)])->errors()->first()
+        );
+        $this->assertSame(
+            'Wrong key.',
+            $validator->make(['input' => 'test'], ['input' => new EnumKey(UserType::class)])->errors()->first()
+        );
+
+        $this->app->setLocale('es');
+
+        $this->assertSame(
+            'The value you have provided is not a valid enum instance.', // No Spanish translations out of the box
+            $validator->make(['input' => 'test'], ['input' => new Enum(UserType::class)])->errors()->first()
+        );
+        $this->assertSame(
+            'Llave incorrecta.',
+            $validator->make(['input' => 'test'], ['input' => new EnumKey(UserType::class)])->errors()->first()
+        );
+    }
+
+    public function test_can_localize_validation_error_message_using_string_rule()
+    {
+        $validator = $this->app['validator'];
+
+        $this->assertSame(
+            'The value you have provided is not a valid enum instance.',
+            $validator->make(['input' => 'test'], ['input' => 'enum:' . UserType::class])->errors()->first()
+        );
+        $this->assertSame(
+            'Wrong key.',
+            $validator->make(['input' => 'test'], ['input' => 'enum_key:' . UserType::class])->errors()->first()
+        );
+
+        $this->app->setLocale('es');
+
+        $this->assertSame(
+            'The value you have provided is not a valid enum instance.', // No Spanish translations out of the box
+            $validator->make(['input' => 'test'], ['input' => 'enum:' . UserType::class])->errors()->first()
+        );
+        $this->assertSame(
+            'Llave incorrecta.',
+            $validator->make(['input' => 'test'], ['input' => 'enum_key:' . UserType::class])->errors()->first()
+        );
     }
 }
---
+++ tests/lang/es/validation.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'enum_key' => 'Llave incorrecta.',
+];
