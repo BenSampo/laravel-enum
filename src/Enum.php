c7ba98dbd6b667c870223ba009f4c2c7d1553fd5
Michael <michael.hurni@gmail.com>
2021-11-22 01:05:15 +0100

    Update toArray & jsonSerialize return type to mixed

--- src/Enum.php
+++ src/Enum.php
@@ -559,7 +559,7 @@ abstract class Enum implements EnumContract, Castable, Arrayable, JsonSerializab
     /**
      * Transform the enum instance when it's converted to an array.
      *
-     * @return string
+     * @return mixed
      */
     public function toArray()
     {
@@ -569,9 +569,9 @@ abstract class Enum implements EnumContract, Castable, Arrayable, JsonSerializab
     /**
      * Transform the enum when it's passed through json_encode.
      *
-     * @return string
+     * @return mixed
      */
-    public function jsonSerialize(): string
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
