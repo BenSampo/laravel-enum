04819a29a34a922c1289cf3794931c044f13ca62
Benedikt Franke <benedikt@franke.tech>
2022-12-07 11:34:06 +0100

    Open `EnumServiceProvider` for customization (#292)

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Types\Type;
 use BenSampo\Enum\Rules\Enum;
 use BenSampo\Enum\Rules\EnumKey;
 use BenSampo\Enum\Rules\EnumValue;
+use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 use Illuminate\Support\ServiceProvider;
 use BenSampo\Enum\Commands\MakeEnumCommand;
 use BenSampo\Enum\Commands\EnumAnnotateCommand;
@@ -23,7 +24,7 @@ class EnumServiceProvider extends ServiceProvider
         $this->bootDoctrineType();
     }
 
-    private function bootCommands(): void
+    protected function bootCommands(): void
     {
         $this->publishes([
             __DIR__.'/Commands/stubs' => $this->app->basePath('stubs')
@@ -37,15 +38,18 @@ class EnumServiceProvider extends ServiceProvider
         }
     }
 
-    private function bootValidators(): void
+    protected function bootValidators(): void
     {
-        $this->app['validator']->extend('enum_key', function ($attribute, $value, $parameters, $validator) {
+        $validationFactory = $this->app->make(ValidationFactory::class);
+        assert($validationFactory instanceof ValidationFactory);
+
+        $validationFactory->extend('enum_key', function ($attribute, $value, $parameters, $validator) {
             $enum = $parameters[0] ?? null;
 
             return (new EnumKey($enum))->passes($attribute, $value);
         }, __('laravelEnum::messages.enum_key'));
 
-        $this->app['validator']->extend('enum_value', function ($attribute, $value, $parameters, $validator) {
+        $validationFactory->extend('enum_value', function ($attribute, $value, $parameters, $validator) {
             $enum = $parameters[0] ?? null;
 
             $strict = $parameters[1] ?? null;
@@ -59,14 +63,14 @@ class EnumServiceProvider extends ServiceProvider
             return (new EnumValue($enum, $strict))->passes($attribute, $value);
         }, __('laravelEnum::messages.enum_value'));
 
-        $this->app['validator']->extend('enum', function ($attribute, $value, $parameters, $validator) {
+        $validationFactory->extend('enum', function ($attribute, $value, $parameters, $validator) {
             $enum = $parameters[0] ?? null;
 
             return (new Enum($enum))->passes($attribute, $value);
         }, __('laravelEnum::messages.enum'));
     }
 
-    private function bootDoctrineType(): void
+    protected function bootDoctrineType(): void
     {
         // Not included by default in Laravel
         if (class_exists('Doctrine\DBAL\Types\Type')) {
@@ -76,12 +80,12 @@ class EnumServiceProvider extends ServiceProvider
         }
     }
 
-    private function bootValidationTranslation(): void
+    protected function bootValidationTranslation(): void
     {
         $this->publishes([
             __DIR__ . '/../lang' => lang_path('vendor/laravelEnum'),
         ], 'translations');
 
-        $this->loadTranslationsFrom(__DIR__ . '/../lang/', 'laravelEnum');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'laravelEnum');
     }
 }
