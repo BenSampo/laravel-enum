af78f61d268101a06d0d633d4a0a813e7b27ca06
tamiroh <tamiroh71@gmail.com>
2023-05-09 00:17:46 +0900

    Remove tests for enums without blank line

--- tests/ArtisanCommandsTest.php
+++ tests/ArtisanCommandsTest.php
@@ -112,18 +112,6 @@ final class ArtisanCommandsTest extends ApplicationTestCase
         $this->assertSame($original, $newClass);
     }
 
-    public function test_annotate_enum_with_multiple_line_comments_without_blank_lines(): void
-    {
-        $filesystem = $this->app->make(Filesystem::class);
-        assert($filesystem instanceof Filesystem);
-
-        $original = $filesystem->get(__DIR__ . '/Enums/EnumWithMultipleLineCommentsWithoutBlankLines.php');
-        $this->artisan('enum:annotate', ['class' => EnumWithMultipleLineCommentsWithoutBlankLines::class])->assertExitCode(0);
-
-        $newClass = $filesystem->get(__DIR__ . '/Enums/EnumWithMultipleLineCommentsWithoutBlankLines.php');
-        $this->assertSame($original, $newClass);
-    }
-
     public function test_annotate_enum_with_single_line_comment_with_blank_line(): void
     {
         $filesystem = $this->app->make(Filesystem::class);
@@ -133,18 +121,6 @@ final class ArtisanCommandsTest extends ApplicationTestCase
         $this->artisan('enum:annotate', ['class' => EnumWithSingleLineCommentWithBlankLine::class])->assertExitCode(0);
 
         $newClass = $filesystem->get(__DIR__ . '/Enums/EnumWithSingleLineCommentWithBlankLine.php');
-        $this->assertSame($original, $newClass);
-    }
-
-    public function test_annotate_enum_with_single_line_comment_without_blank_line(): void
-    {
-        $filesystem = $this->app->make(Filesystem::class);
-        assert($filesystem instanceof Filesystem);
-
-        $original = $filesystem->get(__DIR__ . '/Enums/EnumWithSingleLineCommentWithoutBlankLine.php');
-        $this->artisan('enum:annotate', ['class' => EnumWithSingleLineCommentWithoutBlankLine::class])->assertExitCode(0);
-
-        $newClass = $filesystem->get(__DIR__ . '/Enums/EnumWithSingleLineCommentWithoutBlankLine.php');
         $this->assertSame($original, $newClass);
     }
 }
