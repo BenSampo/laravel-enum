ab08fac6f0719dfff8a2e0eb85a5e74330a04a54
tamiroh <tamiroh71@gmail.com>
2023-05-08 23:23:59 +0900

    Add EnumWithMultipleLineComments2

---
+++ tests/Enums/EnumWithMultipleLineComments2.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace BenSampo\Enum\Tests\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * This is a test enum with multiple line comments.
+ * Test enum with multiple line comments. Test enum
+ * with multiple line comments. Test enum with multiple line comments.
+ * Test enum with multiple line comments.
+ * @method static static A()
+ * @method static static B()
+ * @method static static C()
+ * @extends Enum<int>
+ */
+final class EnumWithMultipleLineComments2 extends Enum
+{
+    const A = 1;
+    const B = 2;
+    const C = 3;
+}
