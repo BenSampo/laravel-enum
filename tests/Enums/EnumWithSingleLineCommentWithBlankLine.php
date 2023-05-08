af1d2539024bac38866e9344e5457a8eca806271
tamiroh <tamiroh71@gmail.com>
2023-05-08 23:41:31 +0900

    Add enums for test

---
+++ tests/Enums/EnumWithSingleLineCommentWithBlankLine.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace BenSampo\Enum\Tests\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * This is a test enum with single line comment.
+ *
+ * @method static static A()
+ * @method static static B()
+ * @method static static C()
+ * @extends Enum<int>
+ */
+final class EnumWithSingleLineCommentWithBlankLine extends Enum
+{
+    const A = 1;
+    const B = 2;
+    const C = 3;
+}
