59859c6e72bad6a6caa16e816484e209be87601c
Benedikt Franke <benedikt.franke@mll.com>
2023-07-21 11:41:10 +0200

    remove unused config

--- tests/Rector/config.php
+++ tests/Rector/config.php
@@ -4,6 +4,5 @@ use BenSampo\Enum\Rector\ToNativeRector;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../rector-config.php');
     $rectorConfig->rule(ToNativeRector::class);
 };
